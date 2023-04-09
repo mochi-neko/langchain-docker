d4d54afe920e7cb2053a3f4f6f55fe7c819fe7df
mochi-neko <t.o.e.4315@gmail.com>
2023-04-09 16:31:53 +0900

    Add index practice

---
+++ langchain/index_practice.py
@@ -0,0 +1,12 @@
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+from langchain.document_loaders import TextLoader
+from langchain.indexes import VectorstoreIndexCreator
+
+loader = TextLoader('./state_of_the_union.txt')
+
+index = VectorstoreIndexCreator().from_loaders([loader])
+
+query = "What did the president say about Ketanji Brown Jackson"
+print(index.query(query))
+
